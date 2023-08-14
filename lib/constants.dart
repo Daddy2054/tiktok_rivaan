4af126ec570324f1a863da860fc10fa21a2b219e
Jean <acalto2@gmail.com>
2023-08-15 00:02:19 +0200

    Bottom Tab Bar

--- lib/constants.dart
+++ lib/constants.dart
@@ -5,6 +5,15 @@ import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:tiktok/controllers/auth_controller.dart';
 
+const pages = [
+  Text('Home Screen'),
+  Text('Search Screen'),
+  Text('Add Page Screen'),
+  Text('Messages Screen'),
+  Text('Profile Screen'),
+];
+
+// COLORS
 const backgroundColor = Colors.black;
 var buttonColor = Colors.red[400];
 const borderColor = Colors.grey;
